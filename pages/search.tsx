07e0169a95d531c3684cbfc50883906cdedd427a
Mehdi <m.abdi.public@gmail.com>
2022-03-21 02:20:17 +0330

    search page appbar

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Container } from "@mui/material";
+import React from "react";
+import ResponsiveNavBar from "../src/partials/Navbar";
+import getAppInfo from "../src/logic/getAppInfo";
 
-export default function search() {
+export default function search(props: any) {
   return (
-    <div>search</div>
-  )
+    <ResponsiveNavBar {...props}>
+      <Container maxWidth="lg">Search box</Container>
+    </ResponsiveNavBar>
+  );
+}
+
+export async function getStaticProps({ locale }: { locale: any }) {
+  const appInfo = await getAppInfo(locale);
+  return {
+    props: { ...appInfo },
+  };
 }
